15d3907e9629babeea3d08bd8e1d848e19d4b65c
brandon_w laptop <bwoo14@my.bcit.ca>
2022-04-07 23:49:26 -0700

    Save button removes buttons and note area
    Save button removes the buttons and note area from the create note area, but does not yet save the note to the object

--- src/app.js
+++ src/app.js
@@ -12,20 +12,10 @@ const note = `<textarea class='new-note' rows="35" cols="50"></textarea>`
 const cancelButtonTemp =  `<button class='cancel'>Cancel</button>`
 const saveButtonTemp = `<button class='save'>Save</button>`
 
-function cancelNote(evt){
-  const note = document.querySelector('.new-note')
-  const save = document.querySelector('.save')
-  const cancel = document.querySelector('.cancel')
-  note.remove()
-  save.remove()
-  cancel.remove()
-  plusButton.addEventListener('click', openNoteSpace)
 
-}
-
-function openNoteSpace(evt){
+function openNoteSpace(evt){ // Opens a blank textarea for note taking
   
-  plusButton.removeEventListener('click', openNoteSpace)
+  plusButton.removeEventListener('click', openNoteSpace) // Ensures that the plus button cannot be clicked again until note saved or cancelled
 
   createNoteArea.insertAdjacentHTML("afterend", cancelButtonTemp)
   createNoteArea.insertAdjacentHTML("afterend", saveButtonTemp)
@@ -33,6 +23,35 @@ function openNoteSpace(evt){
 
   cancelButton = document.querySelector('.cancel')
   cancelButton.addEventListener('click', cancelNote)
+
+  saveButton = document.querySelector('.save')
+  saveButton.addEventListener('click', saveNote)
+}
+
+function cancelNote(evt){
+  const newNote = document.querySelector('.new-note')
+  const save = document.querySelector('.save')
+  const cancel = document.querySelector('.cancel')
+  // Removing cancel button, save button, and the note
+  newNote.remove()
+  save.remove()
+  cancel.remove()
+
+  plusButton.addEventListener('click', openNoteSpace) // Allows for plus button to be clicked again
+
+}
+
+function saveNote(evt) {
+  
+  const newNote = document.querySelector('.new-note')
+  const save = document.querySelector('.save')
+  const cancel = document.querySelector('.cancel')
+
+  newNote.remove()
+  save.remove()
+  cancel.remove()
+
+  plusButton.addEventListener('click', openNoteSpace) // Allows for plus button to be clicked again
 }
 
 plusButton.addEventListener('click', openNoteSpace)
