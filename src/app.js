3ef99c1274fbf8ee3026056c14c5b4b327189d41
brandon_w laptop <bwoo14@my.bcit.ca>
2022-04-07 22:26:26 -0700

    Add note, save, cancel upon button click
    When + button click, adds a space for note taking as well as a save button and a cancel button. Buttons and notes have not been styled and buttons do nothing

--- src/app.js
+++ src/app.js
@@ -5,3 +5,19 @@ const notes = [
     id: 1 
   }
 ]
+
+const plusButton = document.querySelector('.fa-circle-plus')
+const createNoteArea = document.querySelector('.create-note-area')
+const note = `<textarea></textarea>`
+const cancelButton =  `<button>Cancel</button>`
+const saveButton = `<button>Save</button>`
+
+function openNoteSpace(evt){
+  
+  createNoteArea.insertAdjacentHTML("afterend", cancelButton)
+  createNoteArea.insertAdjacentHTML("afterend", saveButton)
+  createNoteArea.insertAdjacentHTML("afterend", note)
+}
+
+
+plusButton.addEventListener('click', openNoteSpace)
