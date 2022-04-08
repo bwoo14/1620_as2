4f793c23ab487234aa07705cece8f82e691b686a
brandon_w laptop <bwoo14@my.bcit.ca>
2022-04-07 23:18:52 -0700

    Add functionality to cancel button
    Add functionalty to cancel button by adding an event listener to it whenever it is clicked. Event listener removes the textarea, save button, and cancel button

--- src/app.js
+++ src/app.js
@@ -8,16 +8,28 @@ const notes = [
 
 const plusButton = document.querySelector('.fa-circle-plus')
 const createNoteArea = document.querySelector('.create-note-area')
-const note = `<textarea></textarea>`
-const cancelButton =  `<button>Cancel</button>`
-const saveButton = `<button>Save</button>`
+const note = `<textarea class='new-note' rows="35" cols="50"></textarea>`
+const cancelButtonTemp =  `<button class='cancel'>Cancel</button>`
+const saveButtonTemp = `<button class='save'>Save</button>`
+
+function cancelNote(evt){
+  const note = document.querySelector('.new-note')
+  const save = document.querySelector('.save')
+  const cancel = document.querySelector('.cancel')
+  note.remove()
+  save.remove()
+  cancel.remove()
+
+}
 
 function openNoteSpace(evt){
   
-  createNoteArea.insertAdjacentHTML("afterend", cancelButton)
-  createNoteArea.insertAdjacentHTML("afterend", saveButton)
+  createNoteArea.insertAdjacentHTML("afterend", cancelButtonTemp)
+  createNoteArea.insertAdjacentHTML("afterend", saveButtonTemp)
   createNoteArea.insertAdjacentHTML("afterend", note)
-}
 
+  cancelButton = document.querySelector('.cancel')
+  cancelButton.addEventListener('click', cancelNote)
+}
 
 plusButton.addEventListener('click', openNoteSpace)
